20e7c94a128c691035d3307423e16a6ffe5abdc1
Gagarock <yael.porthault@outlook.fr>
2024-05-13 11:40:01 +0200

    oui oui oui

--- src/components/Common/BackgroundImage.js
+++ src/components/Common/BackgroundImage.js
@@ -1,12 +1,11 @@
 export default class BackgroundImage extends HTMLElement {
-
     constructor() {
         super();
         this.innerHTML = this.render();
     }
 
     render(){
-        return `<img src="./src/img/slide-1.jpg" class="d-block vh-100 vw-100 object-fit-cover position-fixed" alt="Restaurant slide 1" draggable="false">`;
+        return `<img src="./src/img/slide-1.jpg" class="d-block vh-100 vw-100 object-fit-cover position-fixed" alt="Restaurant slide 1">`;
     }
 }
 customElements.define("background-image", BackgroundImage);
